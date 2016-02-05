cc6714ca84a52a51c02a12c19fba601fa1b84646
Hristo Vrigazov <hvrigazov@gmail.com>
2016-02-05 16:55:04 +0200

    Updated 3rd week assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,51 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Find the inverse matrix by caching results
+# Using the concept "memoization" from functional programming
 
-## Write a short comment describing this function
+# function that exposes the get, set, and getinverse and setinverse
+# functions on the matrix for manipulating the cached result
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    cachedResult <- NULL
+    
+    set <- function (y) {
+        x <<- y
+        cachedResult <<- NULL
+    }
+    
+    get <- function () {
+        x
+    }
+    
+    setinverse <- function (solve) {
+        cachedResult <<- solve
+    }
+    
+    getinverse <- function () {
+        cachedResult
+    }
+    
+    list(set = set,
+         get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## by consuming the api exposed by the function above,
+## cacheSolve finds the inverse of a matrix
+
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    cachedResult <- x$getinverse()
+    
+    # if there exists a cachedResult, return it
+    if (!is.null(cachedResult)) {
+        return(cachedResult)
+    }
+    
+    data <- x$get()
+    cachedResult <- solve(data, ...)
+    x$setinverse(cachedResult)
+    cachedResult
 }
